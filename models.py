89aa223a1d954f9c8e117a88b2f6aee8391d8f45
manivardhan9999 <140253994+manivardhan9999@users.noreply.github.com>
2024-05-21 19:34:32 +0530

    Create models.py

---
+++ models.py
@@ -0,0 +1,13 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    todos = db.relationship('Todo', backref='user', lazy=True)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
